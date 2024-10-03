68a3787166188734e27eaabff907847a4055d53f
CahtarevicAdi <adisfi54@gmail.com>
2024-10-03 12:04:28 +0200

    Ispravka u LoanService klasi: Pravilno referenciranje BorrowerType

--- src/main/java/com/example/librarymanagementsystem/service/LoanService.java
+++ src/main/java/com/example/librarymanagementsystem/service/LoanService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LoanService {
@@ -21,17 +22,54 @@ public class LoanService {
     private ReservationRepository reservationRepository;
 
 
-    public void returnBook(Loan loan, LocalDate returnDate) {
-        loan.setReturnDate(returnDate);
-        loanRepository.save(loan);
+    public void returnBook(Long loanId) {
+
+        Optional<Loan> loanOptional = loanRepository.findById(loanId);
+
+        if (loanOptional.isPresent()) {
+            Loan loan = loanOptional.get();
+
+            LocalDate returnDate = LocalDate.now();
+            loan.setReturnDate(returnDate);
+            loanRepository.save(loan);
 
 
-        List<Reservation> reservations = reservationRepository.findByBookIdAndNotifiedFalse(loan.getBook().getId());
-        if (!reservations.isEmpty()) {
-            Reservation nextReservation = reservations.get(0);
-            nextReservation.setNotified(true);
-            reservationRepository.save(nextReservation);
+            List<Reservation> reservations = reservationRepository.findByBookIdAndNotifiedFalse(loan.getBook().getId());
+            if (!reservations.isEmpty()) {
+                Reservation nextReservation = reservations.get(0);
+                nextReservation.setNotified(true);
+                reservationRepository.save(nextReservation);
 
+
+            }
+        } else {
+            throw new IllegalArgumentException("Posudba sa ID-om " + loanId + " nije pronađena.");
         }
+    }
+
+
+    public void createLoan(Loan loan) {
+        loanRepository.save(loan);
+    }
+
+
+    public boolean extendLoan(Loan loan) {
+
+        if (loan.getBorrower().getBorrowerType() == Borrower.BorrowerType.STANDARD) {
+            if (loan.getExtensions() < 1) {
+                loan.setDueDate(loan.getDueDate().plusDays(7));
+                loan.setExtensions(loan.getExtensions() + 1);
+                loanRepository.save(loan);
+                return true;
+            }
+        } else if (loan.getBorrower().getBorrowerType() == Borrower.BorrowerType.PREMIUM) {
+            if (loan.getExtensions() < 2) {
+                loan.setDueDate(loan.getDueDate().plusDays(10));
+                loan.setExtensions(loan.getExtensions() + 1);
+                loanRepository.save(loan);
+                return true;
+            }
+        }
+        return false;
     }
 }
